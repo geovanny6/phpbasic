58f6217d945a85bffa2b5b6600c72b7d47dc2807
geovanny <geovanny@gmail.com>
2021-01-24 13:09:53 -0500

    mejoramientos

--- resources/views/lists.php
+++ resources/views/lists.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Descargar productos</title>
+    <title>Descargar productos en pdf</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
@@ -22,7 +22,7 @@
                 ?>
                 <hr>
                 <p><a href="pdf.php">Exportar en pdf</a></p>
-                <span class="author">GEovanny Orellana</span>
+                <span class="author">Geovanny Orellana mira mas información en  <a href="https://geoavanny6.wordpress.com">Geovanny blog</a> </span>
             </div>
         </div>
     </div>
